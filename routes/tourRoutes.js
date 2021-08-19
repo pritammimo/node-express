3be5a7d66ba932fe05dada7b94e65c0d67e21c6b
pritam <pritam.saha@webskitters.com>
2021-08-19 20:14:40 +0530

    aggregation and pipeline

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,13 +7,18 @@ const {
   getTour,
   updateTour,
   deleteTour,
+  aliasTopTours,
+  getTourStats,
+  getMonthlyPlan,
   // checkID,
 } = tourController;
 const router = express.Router();
 
 // router.param('id', checkID);
 //Create a checkbody middlewarre
-
+router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
+router.route('/tour-stats').get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 module.exports = router;
