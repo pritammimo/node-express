f771505dd3d8d1e31427652211a7fe890f0b640c
pritam <pritam.saha@webskitters.com>
2021-08-20 16:58:09 +0530

    async Handler

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -17,13 +19,18 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋 ');
-  next();
-});
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+//For error handling
+app.all('*', (req, res, next) => {
+  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+//Global Middleware for errorhandling
+app.use(globalErrorHandler);
 module.exports = app;
