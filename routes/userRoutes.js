074d002a47b4eb80db029ce7049922a16977ed2a
pritam <pritam.saha@webskitters.com>
2021-08-20 22:45:52 +0530

    jwt add

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,9 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 const { getAllUsers, createUser, getUser, updateUser, deleteUser } =
   userController;
+const { signup } = authController;
+router.post('/signup', signup);
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
