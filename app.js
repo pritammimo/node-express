f2f4e06f2aa9ca9564362c0f9399abe99ca4b31a
pritam <pritam.saha@webskitters.com>
2021-08-23 13:37:16 +0530

    api-security-complete

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -7,15 +12,45 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-//MiDDLEWARS
+// Global MiDDLEWARS
+// set Security http headers
+app.use(helmet());
 if (process.env.NODE_ENV === 'develoment') {
   app.use(morgan('dev'));
 }
+//limit request from same api
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this ip,please try again after 1 hour',
+});
+
+app.use('/api', limiter);
+
 app.use(morgan('dev'));
-
-app.use(express.json());
-
-//for static files
+//Body-parser
+app.use(
+  express.json({
+    limit: '10kb',
+  })
+);
+//Data Sanitzation against Nosql query injection
+app.use(mongoSanitize());
+//Data sanitazation against xss
+app.use(xss());
+//Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+//Serving static file
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
