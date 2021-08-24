70a14f265ccb283c6821104446777b0cada6bbb8
pritam <pritam.saha@webskitters.com>
2021-08-24 21:59:42 +0530

    delete factory dunction

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,5 +1,6 @@
 const Review = require('../models/reviewModel');
 const catchAsync = require('../utils/catchAsync');
+const factory = require('./handlerFactory');
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
   let filter = {};
@@ -31,3 +32,4 @@ exports.createReview = catchAsync(async (req, res, next) => {
     },
   });
 });
+exports.deleteReview = factory.deleteOne(Review);
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { protect, restrictTo } = require('../controllers/authController');
 const reviewController = require('../controllers/reviewController');
 
+const { deleteReview } = reviewController;
 const { getAllReviews, createReview } = reviewController;
 const router = express.Router({ mergeParams: true });
 
@@ -10,3 +11,4 @@ router
   .get(getAllReviews)
   .post(protect, restrictTo('user'), createReview);
 module.exports = router;
+router.route('/:id').delete(deleteReview);
