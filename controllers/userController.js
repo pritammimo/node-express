9bc046be5a91419cd8f3f11f95cd6ea1c4f037d6
pritam <pritam.saha@webskitters.com>
2021-08-25 13:05:49 +0530

    handler factory

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,18 +11,6 @@ const filterObj = (obj, ...allowedFields) => {
   return newObj;
 };
 
-exports.getAllUsers = catchAsync(async (req, res, next) => {
-  const users = await User.find();
-
-  //SEND RESPONSE
-  res.status(200).json({
-    status: 'success',
-    results: users.length,
-    data: {
-      users,
-    },
-  });
-});
 exports.updateMe = catchAsync(async (req, res, next) => {
   //1)Create error if user posts password data
   if (req.body.password || req.body.passwordConfirm) {
@@ -49,22 +37,16 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
     data: null,
   });
 });
-exports.getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
+
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'This route is not yet defined',
+    message: 'This route is not  defined! plz use signup ',
   });
 };
-exports.updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
+//Do not update user password with this
+
+exports.updateUser = factory.updateOne(User);
+exports.getAllUsers = factory.getAll(User);
+exports.getUser = factory.getOne(User);
 exports.deleteUser = factory.deleteOne(User);
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,13 +2,13 @@ const express = require('express');
 const { protect, restrictTo } = require('../controllers/authController');
 const reviewController = require('../controllers/reviewController');
 
-const { deleteReview } = reviewController;
-const { getAllReviews, createReview } = reviewController;
+const { deleteReview, updateReview, getReview } = reviewController;
+const { getAllReviews, createReview, setTourUserIds } = reviewController;
 const router = express.Router({ mergeParams: true });
 
 router
   .route('/')
   .get(getAllReviews)
-  .post(protect, restrictTo('user'), createReview);
+  .post(protect, restrictTo('user'), setTourUserIds, createReview);
 module.exports = router;
-router.route('/:id').delete(deleteReview);
+router.route('/:id').get(getReview).delete(deleteReview).patch(updateReview);
